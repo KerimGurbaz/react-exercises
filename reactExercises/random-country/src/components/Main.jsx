627242c05d7804afdd5bce673348d421214bce90
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-24 11:56:55 +0200

    router

---
+++ reactExercises/random-country/src/components/Main.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import axios from "axios";
+import Card from "./Card";
+
+
+
+const Main = () => {
+    const [res, setRes] = useState("");
+
+    const url = "https://restcountries.com/v3.1/all";
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Main
