8384d28751ea0ac20ab2650a9f0567eeaf1708c1
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-31 17:59:23 +0100

    updated

--- reactExercises/random-country/src/components/Card.jsx
+++ reactExercises/random-country/src/components/Card.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Card = ({ props }) => {
-  const { flag, capital, population } = props;
+  const { flag, capital, population, name: common } = props;
+  console.log(flag);
   return (
-    <div>
-      <div> {flag}</div>
+    <div className="country-div">
+      <div className="img">{flag}</div>
+
       <h2>Capital : {capital}</h2>
       <h3>Population : {population}</h3>
     </div>
--- reactExercises/random-country/src/components/Main.jsx
+++ reactExercises/random-country/src/components/Main.jsx
@@ -4,15 +4,21 @@ import Card from "./Card";
 
 const Main = () => {
   const [country, setCountry] = useState([]);
+  const [name, setName] = useState(0);
+
   useEffect(() => {
     axios.get(`https://restcountries.com/v3.1/all`).then((res) => {
-      setCountry(res.data[0]);
+      setCountry(res.data[Math.floor(Math.random() * res.data.length)]);
+      console.log(res.data.length);
+      console.log("icerde calistim");
+      console.log(country);
     });
   }, []);
 
   return (
     <div>
       <Card props={country} />
+      <button>Click</button>
     </div>
   );
 };
